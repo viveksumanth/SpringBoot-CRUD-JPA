237fdb90c2c997454f7dac0b64948136c8d65c34
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2024-11-09 16:13:56 -0800

    Revert "updated properties (#13)" (#14)
    This reverts commit 579d98f359fcf4f9b04eecd2b98cbd143a67ad66.

--- src/main/java/com/dev/crudrestapisql/dao/CompanyRepository.java
+++ src/main/java/com/dev/crudrestapisql/dao/CompanyRepository.java
@@ -3,5 +3,6 @@ package com.dev.crudrestapisql.dao;
 import com.dev.crudrestapisql.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path="companies")
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
 }
