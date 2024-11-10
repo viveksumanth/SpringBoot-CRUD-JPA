e9ce257a54518e2644bb3800271a4bd8e8b07574
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2024-11-09 16:16:23 -0800

    Update properties (#15)
    * push check
    
    * Merging changes from Dev to Main (#5)
    
    * corrected class names
    
    * verify push
    
    * check push again
    
    * testing push (#4)
    
    * Test2 (#7)
    
    * rebasing
    
    * remove EOF empty line
    
    * Test2 (#7)
    
    * rebasing
    
    * remove EOF empty line
    
    * Update application.properties (#8)
    
    * Dev to Main changes (#11)
    
    * corrected class names
    
    * verify push
    
    * check push again
    
    * testing push (#4)
    
    * Test2 (#7)
    
    * rebasing
    
    * remove EOF empty line
    
    * Test2 (#7)
    
    * rebasing
    
    * remove EOF empty line
    
    * Update application.properties (#8)
    
    * added Pull request template (#9)
    
    * Feature/add commit message directory (#10)
    
    * added Pull request template
    
    * updated pull request template
    
    * updated properties

--- src/main/java/com/dev/crudrestapisql/dao/CompanyRepository.java
+++ src/main/java/com/dev/crudrestapisql/dao/CompanyRepository.java
@@ -3,6 +3,5 @@ package com.dev.crudrestapisql.dao;
 import com.dev.crudrestapisql.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//@RepositoryRestResource(path="companies")
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
 }
